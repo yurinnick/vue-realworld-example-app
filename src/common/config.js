c33accfaabc86ecb7bae9ed2ed75cbbfcaf48fb8
Nikolay Yurin <nyurin@griddynamics.com>
2018-11-17 00:48:26 -0800

    Lint autofix

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,3 @@
-export const API_URL = process.env.VUE_APP_API_URL || "http://localhost:3000/api";
+export const API_URL =
+  process.env.VUE_APP_API_URL || "http://localhost:3000/api";
 export default API_URL;
